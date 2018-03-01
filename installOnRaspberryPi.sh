b53df09d2f8078e78a230ec26773d81db24556ec
Tim Franssen <tim.franssen@nedap.com>
2018-03-01 23:52:13 +0100

    Installation has changed for the newer node version

--- installOnRaspberryPi.sh
+++ installOnRaspberryPi.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# Run this as the normal "pi" user, it will escalate where necessary
+
+echo "Please enter the root password when asked, default is 'raspberry'"
+
+# Make sure the PI is up to date
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install nodejs npm
 
+# Allow the "pi" user to control the usb devices
+sudo chmod 222 /sys/bus/usb/drivers/usb/unbind
+sudo chmod 222 /sys/bus/usb/drivers/usb/bind
+
+# Install nvm, node and npm
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+
+# Install the package dependencies
 npm install
-
